09bbe41e763f8b543cdd09a0c931c738a7741626
huyaocode <2385249676@qq.com>
2018-10-29 16:31:51 +0800

    添加回到首页a标签

--- staticServer/src/index.js
+++ staticServer/src/index.js
@@ -12,17 +12,14 @@ http
       pathname = '../src/index.html'
     }
     fs.readFile(path.normalize('../static/' + pathname), (err, data) => {
-      console.log('data   ',data)
       //404页面
       if (err || data == undefined) {
-        console.log('com')
         fs.readFile('../src/404.html', function(err, data) {
           res.writeHead(404, { 'Content-Type': 'text/html;charset=UTF8' })
           res.end(data)
         })
       } else {
         let type = mime(pathname)
-        console.log(pathname)
         res.writeHead(200, { 'Content-Type': type })
         res.end(data)
       }
